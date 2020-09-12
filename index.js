d3dd1ba323d8b8392d5f128d8f2315856d3fc5a9
Richard Dixon <43769645+dontbecypher16@users.noreply.github.com>
2020-09-12 05:42:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 // edit the TipCalculator function so it has two
 // parameters, mealCost and tipPercent
 // instead of the hard coded 20% (0.2)
-function tipCalculator(mealCost) {
-  return mealCost * 0.2;
+let mealCost;
+let tipPercent;
+function tipCalculator(mealCost, tipPercent) {
+  return mealCost * tipPercent;
 }
+
 let tipInput = document.querySelector("#tipAmount");
+let tipPercent = tipInput;
 // get the user input out of the 1st text box
 // give a variable name to the 1st text box
 let userInput = document.querySelector("#mealAmount");
+let mealCost = userInput;
 // pay attention to changes in userInput
-userInput.addEventListener("change", function () {
+userInput.addEventListener("change", function(tips) {
+
   console.log(userInput.value);
   // run the tipCalculator function
   // using the number the user entered for the meal
   let newTip = tipCalculator(userInput.value);
   // show that tip in the 2nd text box
   tipInput.value = newTip;
-});
+  var output = "";
+  for (const entry of newTip) {
+    output = output + entry[0] + "=" + entry[4] + "\r";
+  };
+
+  tips.innerText = output;
+  event.preventDefault();
+},false);
+
+
